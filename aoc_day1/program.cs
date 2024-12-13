6a4817d30dc2041d965f453c9438c0d313a61b83
Robin <142886484+RobinTea@users.noreply.github.com>
2024-12-13 16:58:23 +0100

    Create program.cs

---
+++ aoc_day1/program.cs
@@ -0,0 +1,14 @@
+namespace aoc24_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:/Users/RinSk/Desktop/sprite/workspace/BBB/aoc/input.txt";
+
+            //1-2
+            int totalSame = aocEinsZwei.pEinsZwei(filePath);
+            Console.WriteLine(totalSame);
+        }
+    }
+}
